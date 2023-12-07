8e01005050fbb363c9a2baa15201d3acb904f2dc
vanquang297 <37823197+vanquang297@users.noreply.github.com>
2023-12-07 08:56:01 +0700

    up

---
+++ schedules.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2023 at 02:53 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hoidanit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` int(11) NOT NULL,
+  `currentNumber` int(11) DEFAULT NULL,
+  `maxNumber` int(11) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL,
+  `timeType` varchar(255) DEFAULT NULL,
+  `doctorId` int(11) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `schedules`
+--
+
+INSERT INTO `schedules` (`id`, `currentNumber`, `maxNumber`, `date`, `timeType`, `doctorId`, `createdAt`, `updatedAt`) VALUES
+(1, NULL, 10, '1701968400000', 'T1', 14, '2023-12-07 08:14:41', '2023-12-07 08:14:41'),
+(2, NULL, 10, '1701968400000', 'T2', 14, '2023-12-07 08:14:41', '2023-12-07 08:14:41'),
+(3, NULL, 10, '1701968400000', 'T3', 14, '2023-12-07 08:14:41', '2023-12-07 08:14:41');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
